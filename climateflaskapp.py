ef91a2882ee1a6913e6f0cd7def549ee1030dcd2
Ly Li <76023384+lyli888@users.noreply.github.com>
2021-03-13 19:32:34 -0500

    Update climateflaskapp.py

--- climateflaskapp.py
+++ climateflaskapp.py
@@ -5,6 +5,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+import datetime as dt
 
 import pandas as pd
 import numpy as np
@@ -46,50 +47,57 @@ def precipitation():
     #Query Precipitation
     precipitation_results = session.query(Measurement.prcp).all()
 
+    #Close Session
     session.close()
     
     #Convert the query results to a dictionary using `date` as the key and `prcp` as the value.
 
-    all_precipitation = []
+    precipitation_dict = {}
+    date_list = []
+    prcp_list = []
+    
     for date in precipitation_results:
-        precipitation_dict = {}
-        passenger_dict["date"] = date
-        passenger_dict["prcp"] = prcp
-        all_precipitation.append(passenger_dict)
+        
+        current_date = precipitation_results["date"] 
+        current_prcp = int(precipitation_results["prcp"])
+        
+        precipitation_dict = precipitation_dict.append{current_date:current_prcp}
 
-    return jsonify(all_precipitation)
+    return jsonify(precipitation_dict)
 
 #List Stations
 @app.route("/api/v1.0/stations")
 def stations():
-
     
     session = Session(engine)
-    
     station_results = session.query(Station.station, Station.name).all()
-    
     session.close()
     
     return jsonify(station_results)
 
+#Temperature Past Year
 @app.route("/api/v1.0/tobs")
 def tobs():
     
-    
     session = Session(engine)
-    
     prev_year_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
-    
+    prev_year_date = dt.date(prev_year_date) - dt.timedelta(days=365)
     tobs_results = session.query(Measurement.date, Measurement.station, Measurement.tobs).filter(Measurement.date >= prev_year_date).all()
-    
     session.close()
   
     return jsonify(tobs_results)
 
+#Start
 @app.route("/api/v1.0/<start>")
 def start(date):
     start_results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= date).all()
     return jsonify(day_temp_results)
+
+#Start/End
+@app.route("/api/v1.0/<start>/<end>")
+def startend(start,end):
+    start_end_results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    return jsonify(multi_day_temp_results)
 
 if __name__ == '__main__':
     app.run(debug=True)
